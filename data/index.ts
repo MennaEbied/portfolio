900e9b547dbe29bfc320eb09875969c65131e0bf
MennaEbied <mennahesham253@gmail.com>
2025-10-24 02:44:17 +0300

    add data

---
+++ data/index.ts
@@ -0,0 +1,89 @@
+export const navItems = [
+    { name: "About", link: "#about" },
+    { name: "Projects", link: "#projects" },
+    { name: "Contact", link: "#contact" },
+  ];
+  
+  export const gridItems = [
+    {
+      id: 1,
+      title: "Bachelor's of Electronics and Communication Engineering 2020-2025",
+      description: "Education",
+      className:
+      "lg:translate-y-2 lg:col-span-3 md:col-span-7 md:row-span-4 lg:min-h-[70vh] rounded-3xl overflow-hidden relative bg-gradient-to-br from-[#0f172a] via-[#1e293b] to-[#0f172a] shadow-xl text-white",
+      imgClassName: "w-full h-full object-cover opacity-25",
+      titleClassName: "justify-center items-center text-center",
+      img: "/bg.png",
+      spareImg: "",
+    },
+    {
+      id: 2,
+      title: "I'm very flexible with time zone communications",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 3,
+      title: "My tech stack",
+      description: "I constantly try to improve",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-center",
+      img: "",
+      spareImg: "",
+    },
+  ];
+  
+  
+  
+  export const projects = [
+    {
+      id: 1,
+      title: "Movie Discovery Website",
+      des: "A dynamic movie discovery platform that fetches real-time data from the TMDb API. The application features instant search and filtering functionalities for an improved user experience. It was built with an intuitive, 100% mobile-responsive UI using Tailwind CSS and uses React Router for seamless, single-page navigation.",
+      img: "/project1.png",
+      iconLists: ["/re.svg", "/tail.svg", "/ts.svg"],
+      link: "https://movies-react-3qj.pages.dev",
+    },
+    {
+      id: 2,
+      title: "Sushi Restaurant Website",
+      des: "A modern, responsive e-commerce website for a sushi restaurant. The platform features secure user sign-up and login flows using Firebase Authentication and includes persistent shopping cart system powered by Redux and Redux Toolkit. ",
+      img: "/project2.png",
+      iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/Firebase.svg","/Redux.svg"],
+      link: "https://react-restaurant-website.pages.dev/",
+    },
+    {
+      id: 3,
+      title: "Parking Management Dashboard",
+      des: "An admin dashboard designed to optimize parking space utilization and track revenue in real-time...",
+      img: "/project3.png",
+      iconLists: ["/re.svg", "/Sass.svg", "/MUI.svg","/Firebase.svg"],
+      link: "https://car-parking-frontend-eight.vercel.app",
+    },
+    {
+      id: 4,
+      title: "Weather App",
+      des: "A React Native weather app built with Expo, using TypeScript, expo-router for navigation, and the WeatherAPI for real-time weather data ",
+      img: "/project4.png",
+      iconLists: ["/re.svg", "/tail.svg", "/ts.svg"],
+      link: "https://github.com/MennaEbied/weatherApp-react-native",
+    },
+  ];
+  
+  export const socialMedia = [
+    {
+      id: 1,
+      img: "/git.svg",
+      link:'https://github.com/MennaEbied'
+    },
+    {
+      id: 3,
+      img: "/link.svg",
+      link:'https://www.linkedin.com/in/menna-hesham-318b92260'
+    },
+  ];
